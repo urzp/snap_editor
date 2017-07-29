38b36c032fce6fbfd2988ac71df68dc13fdc27f1
urzp <urzp002@mail.ru>
2017-07-29 22:50:25 +0300

    creating citcle editor

---
+++ js/el_frame.js
@@ -0,0 +1,110 @@
+dw_frame ={
+    nodes:{},
+    attr_nodes:{     
+        fill:"#FFF",
+        stroke:"#30839b",
+        strokeWidth: '2'
+    },
+    edges:{},
+    attr_edges:{
+       stroke: "#30839b",
+       strokeWidth: '1', 
+       'stroke-dasharray': '5,5' 
+    },
+    inited: false,
+    current_el: null,
+    box: {}
+}
+
+dw_frame.init = function(){
+    this.edges.top = snap.line();
+    this.edges.right = snap.line();
+    this.edges.bottom = snap.line();
+    this.edges.left = snap.line();
+    $.each(this.edges,function(index,val){
+        val.attr(dw_frame.attr_edges) 
+    })
+    
+    this.nodes.left_top = snap.circle();
+    this.nodes.right_top = snap.circle();
+    this.nodes.right_bottom = snap.circle();
+    this.nodes.left_bottom = snap.circle();
+    $.each(this.nodes,function(index,val){
+        val.attr(dw_frame.attr_nodes)
+        val.attr({id: "node_"+index})
+        val.drag(dw_frame.moveFunc)
+    })
+    
+    
+    this.inited = true;
+}
+
+dw_frame.draw =  function(element){
+    dw_frame.current_el = element
+    if (dw_frame.inited == false){dw_frame.init()}
+    var box = element.getBBox();
+    this.box = box;
+    this.nodes.left_top.attr({cx:box.x,cy:box.y, r:4 })   
+    this.nodes.right_top.attr({cx:box.x+box.w,cy:box.y, r:4 })
+    this.nodes.right_bottom.attr({cx:box.x+box.w,cy:box.y+box.h, r:4 })
+    this.nodes.left_bottom.attr({cx:box.x,cy:box.y+box.h, r:4 }) 
+    
+    this.edges.top.attr({ x1:box.x, y1:box.y, x2:box.x+box.w, y2:box.y})
+    this.edges.right.attr({ x1:box.x+box.w, y1:box.y, x2:box.x+box.w, y2:box.y+box.h })
+    this.edges.bottom.attr({ x1:box.x+box.w, y1:box.y+box.h, x2:box.x, y2:box.y+box.h })
+    this.edges.left.attr({ x1:box.x, y1:box.y+box.h, x2:box.x, y2:box.y })
+       
+}
+
+dw_frame.moveFunc = function (dx, dy, posx, posy) {
+    posx = posx - parseInt(  $( "#svg" ).offset().left )
+    posy = posy - parseInt(  $( "#svg" ).offset().top )
+    this.attr( { cx: posx , cy: posy } ); 
+    dw_frame.trasform_el(this)
+};
+
+dw_frame.remove = function(){
+    this.current_el = null;
+    $.each(this.edges,function(index,val){
+        val.remove()
+    })
+    $.each(this.nodes,function(index,val){
+        val.remove()
+    })
+    this.inited = false;
+}
+
+dw_frame.trasform_el = function(move_node){
+    var element = this.current_el
+    if (element.type == "line"){
+        switch( move_node.attr("id") ){
+            case  "node_left_top":
+                element.attr({
+                    x1: this.nodes.left_top.attr("cx"),
+                    y1: this.nodes.left_top.attr("cy")
+                })
+                break
+            case "node_right_bottom":
+                element.attr({
+                    x2: this.nodes.right_bottom.attr("cx"),
+                    y2: this.nodes.right_bottom.attr("cy")
+                })
+                break
+            case "node_left_bottom":
+                element.attr({
+                    x1: this.nodes.left_bottom.attr("cx"),
+                    y1: this.nodes.left_bottom.attr("cy")
+                })
+                break
+            case "node_right_top":
+                element.attr({
+                    x2: this.nodes.right_top.attr("cx"),
+                    y2: this.nodes.right_top.attr("cy")
+                })
+        }        
+   }
+  
+    dw_frame.draw(element)
+}
+
+
---
+++ js/main.js
@@ -0,0 +1,147 @@
+behavior = {}
+behavior.draw = true
+behavior.type = "circle"
+
+
+ 
+
+$(document).ready(function(){
+
+    snap = Snap("#svg");
+    
+    
+
+    
+    $( "#svg" ).click(function(event){    
+       //behavior.draw = false     
+    })
+    
+    
+    
+    $("#svg").mousemove(function(event){
+        if ((event.which == 1)&&(behavior.draw == true)) {
+            draw_end(active_figure)
+            get_xy()
+        } 
+    })
+    
+
+    
+        
+    $("#svg").mousedown(function( event ){
+        if (behavior.draw == true){
+            if (behavior.type == 'line'){active_figure = draw_line()}
+            if (behavior.type == 'circle'){active_figure = draw_circle()} 
+        }
+         
+        
+    }) 
+        
+   
+    
+
+    
+    $( "#svg" ).dblclick(function(){
+        
+        draw_frame(active_figure)
+    })
+
+
+    
+})
+
+
+
+function get_xy(){
+    var x_el = parseInt(  $( "#svg" ).offset().left )
+    var y_el = parseInt(  $( "#svg" ).offset().top )
+    var x = event.pageX - x_el
+    var y = event.pageY - y_el
+    return {x: x, y: y}
+}
+
+function draw_end (element){ 
+        
+        if (element.type == "line"){
+            element.attr({
+                x2: get_xy().x,
+                y2: get_xy().y
+           })      
+        }
+        if (element.type == "circle"){
+          var cx = parseInt(active_figure.attr("cx")) 
+          var cy = parseInt(active_figure.attr("cy")) 
+          var x = get_xy().x
+          var y = get_xy().y
+          if (cx>x){var rx = cx - x}else{var rx = x - cx }
+          if (cy>y){var ry = cy - y}else{var ry = y - cy }
+          var r = Math.sqrt(Math.pow(rx,2)+Math.pow(ry,2))
+          element.attr({
+              r:r
+          })
+        }
+        
+
+       dw_frame.draw(element); 
+}
+
+function draw_line(){
+    var line = snap.line(get_xy().x, get_xy().y, get_xy().x, get_xy().y);   
+    line.attr({
+        stroke: "#000",
+        strokeWidth: '2',      
+    })     
+    return line;
+}
+
+function draw_circle(){
+    var circle = snap.circle(get_xy().x, get_xy().y,4); 
+    circle.attr({
+        fill:"#FFF",
+        stroke:"#000",
+        strokeWidth: '2'
+    })
+    return circle;
+}
+
+/*
+
+
+    
+    $( "#svg" ).click(function(event){    
+             
+    })
+    
+    
+    
+    $("#svg").mousemove(function(event){
+        if (event.which == 1) {
+            draw_end(active_figure)
+        }else{
+            remove_frame(active_figure)
+        }    
+    })
+    
+
+        
+    $("#svg").mousedown(function( event ){
+        
+        active_figure = draw_line();  
+        
+    }) 
+        
+   
+    
+
+    
+    $( "#svg" ).dblclick(function(){
+        
+        draw_frame(active_figure)
+    })
+    
+  */ 
+
+
+    //drw_box.remove()
+    //line.transform("r90 200 150")
+    //line.data('name',"first_line")
